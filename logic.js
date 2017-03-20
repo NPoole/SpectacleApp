e8dc16557d079989fab07c5926e4e60922f3a3e4
Nick Poole <Nick.Poole@SparkFun.com>
2017-03-20 16:22:26 -0600

    Update logic.js

--- logic.js
+++ logic.js
@@ -883,6 +883,7 @@ $(document).on("change", ".color", function() {
 $(".number").val(null);
 
 $(document).on("change", ".channel", function() {
+    $(this).val(Math.round($(this).val));
   if (parseInt($(this).val(), 10) > 63) {
     $(this).val(63);
   }else if(parseInt($(this).val(), 10) < 0){
@@ -891,6 +892,7 @@ $(document).on("change", ".channel", function() {
 });
 
 $(document).on("change", ".light-channel", function() {
+    $(this).val(Math.round($(this).val));
   if (parseInt($(this).val(), 10) > 2) {
     $(this).val(2);
   }else if(parseInt($(this).val(), 10) < 0){
@@ -899,6 +901,7 @@ $(document).on("change", ".light-channel", function() {
 });
 
 $(document).on("change", ".light-count", function() {
+    $(this).val(Math.round($(this).val));
   if (parseInt($(this).val(), 10) > 60) {
     $(this).val(60);
   }else if(parseInt($(this).val(), 10) < 1){
@@ -907,6 +910,7 @@ $(document).on("change", ".light-count", function() {
 });
 
 $(document).on("change", ".pixel-number", function() {
+    $(this).val(Math.round($(this).val));
   if (parseInt($(this).val(), 10) > 59) {
     $(this).val(59);
   }else if(parseInt($(this).val(), 10) < 0){
@@ -915,6 +919,7 @@ $(document).on("change", ".pixel-number", function() {
 });
 
 $(document).on("change", ".button-channel", function() {
+    $(this).val(Math.round($(this).val));
   if (parseInt($(this).val(), 10) > 8) {
     $(this).val(8);
   }else if(parseInt($(this).val(), 10) < 0){
@@ -923,6 +928,7 @@ $(document).on("change", ".button-channel", function() {
 });
 
 $(document).on("change", ".servo-channel", function() {
+    $(this).val(Math.round($(this).val));
   if (parseInt($(this).val(), 10) > 4) {
     $(this).val(4);
   }else if(parseInt($(this).val(), 10) < 0){
@@ -931,6 +937,7 @@ $(document).on("change", ".servo-channel", function() {
 });
 
 $(document).on("change", ".file-number", function() {
+    $(this).val(Math.round($(this).val));
   if (parseInt($(this).val(), 10) > 99) {
     $(this).val(99);
   }else if(parseInt($(this).val(), 10) < 0){
@@ -939,6 +946,7 @@ $(document).on("change", ".file-number", function() {
 });
 
 $(document).on("change", ".in-channel", function() {
+    $(this).val(Math.round($(this).val));
   if (parseInt($(this).val(), 10) > 63) {
     $(this).val(63);
   }else if(parseInt($(this).val(), 10) < 0){
