8936ee6debde6928255e005f2d24c2654a97795b
Nick Poole <Nick.Poole@SparkFun.com>
2017-03-20 15:17:10 -0600

    Update logic.js

--- logic.js
+++ logic.js
@@ -196,8 +196,8 @@ function cacheSave(){
 
 function newScript() {
     var makeSure = confirm("Starting a new script will erase your current progress!\n\
-	If you want to keep your progress, press Cancel and then Save or Share your project.\n\
-	To start a new script, press OK.");
+If you want to keep your progress, press Cancel and then Save or Share your project.\n\
+To start a new script, press OK.");
     if(makeSure==true){
     localStorage.setItem("cached", null);
     window.location.href = 'https://npoole.github.io/SpectacleApp/';}	
